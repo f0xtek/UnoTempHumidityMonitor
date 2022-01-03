862ce90bf030bc8acd27836ef5e898d6c9263508
Luke Anderson <landerson61@gmail.com>
2022-01-03 13:46:29 +0000

    Add code to monitor temp & humidity
    - PlatformIO project config
    - Required libraries
    - Source code to monitor temperature & humidity

---
+++ README.md
@@ -0,0 +1,16 @@
+# Arduino Uno Temperature & Humidity Monitor
+
+Simple project to monitor the temperature & humidity of the environment, using an Arduino Uno & LCD display.
+
+> Note: this project utilises the VS Code PlatformIO plugin for project management.
+
+## Required Parts
+
+- Arduino Uno R3
+- DHT11 temperature & humidity sensor
+- LCD1602 16x2 Liquid Crystal Display module
+- 10 kOhm Potentiometer
+
+## Wiring Schematic
+
+TODO: add wiring schematic
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <LiquidCrystal.h> // Ardunio LiquidCrystal library
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+/*
+ * Initialize ther LCD object, passing in the reset & enable pins,
+ * as well as the data pins in use
+ * lcd(resetPin, enablePin, data1, data2, data3, data4)
+ */
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+/*
+ * Initialize the DHT sensor object
+ */
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup()
+{
+  dht.begin();
+  lcd.begin(16, 2); // columns, rows
+  lcd.print("Hello, Luke!");
+}
+
+void loop()
+{
+  // wait between measurements
+  delay(5000);
+
+  float h = dht.readHumidity();
+  float tc = dht.readTemperature(); // default celsius
+
+  /*
+   * check for valid readings from DHT sensor
+   */
+  if (isnan(h) || isnan(tc))
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("DHT read failed!");
+    return;
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: " + String(tc) + " C");
+  lcd.setCursor(0, 1);
+  lcd.print("Humidity: " + String(h) + "%");
+}
